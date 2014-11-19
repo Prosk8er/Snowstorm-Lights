aa6a39e24cd8c7c8d4759e9ec1a3bf789e9c6379
Prosk8er <prosk8er@gotskillslounge.com>
2014-11-18 19:11:50 -0500

    update acp module

--- ext/prosk8er/snowstormlights/acp/snowstorm_lights_module.php
+++ ext/prosk8er/snowstormlights/acp/snowstorm_lights_module.php
@@ -19,28 +19,43 @@ class snowstorm_light_module
 		global $db, $user, $auth, $template, $cache, $request;
 		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
 
-		$user->add_lang('acp/common');
-		$this->user->add_lang_ext('prosk8er/snowstormlights', 'info_acp_snowstorm_lights');
-		$this->tpl_name = 'snowstorm_lights';
-		$this->page_title = $user->lang['CHRISTMAS_LIGHTS_MOD'];
-		add_form_key('acp_snowstorm_lights');
+		// Add the common lang file
+		$this->user->add_lang(array('acp/common'));
 
-		if ($request->is_set_post('submit'))
+		// Add the board snowstormlights ACP lang file
+		$this->user->add_lang_ext('prosk8er/snowstormlights', 'info_acp_snowstorm_lights');
+
+		// Load a template from adm/style for our ACP page
+		$this->tpl_name = 'snowstorm_lights';
+
+		// Set the page title for our ACP page
+		$this->page_title = $user->lang['CHRISTMAS_LIGHTS_MOD'];
+
+		// Define the name of the form for use as a form key
+		$form_name = 'acp_snowstorm_lights';
+		add_form_key($form_name);
+
+		// If form is submitted or previewed
+		if ($this->request->is_set_post('submit'))
 		{
-			if (!check_form_key('acp_snowstorm_lights'))
+			// Test if form key is valid
+			if (!check_form_key($form_name))
 			{
 				trigger_error('FORM_INVALID');
 			}
 
+			// Store the config enable/disable state
 			$config->set('scl_enabled', $request->variable('scl_enabled', 0));
 			$config->set('snow_enabled', $request->variable('snow_enabled', 0));
 
-			trigger_error($user->lang('SNOWSTORM_LIGHTS_SAVED') . adm_back_link($this->u_action));
+			// Output message to user for the update
+			trigger_error($this->user->lang('SNOWSTORM_LIGHTS_SAVED') . adm_back_link($this->u_action));
 		}
 
-		$template->assign_vars(array(
-			'SCL_ENABLED'		=> $config['scl_enabled'],
-			'SNOW_ENABLED'		=> $config['snow_enabled'],
+		// Output data to the template
+		$this->template->assign_vars(array(
+			'SCL_ENABLED'		=> $this->config['scl_enabled'],
+			'SNOW_ENABLED'		=> $this->config['snow_enabled'],
 			'U_ACTION'		=> $this->u_action,
 		));
 	}
