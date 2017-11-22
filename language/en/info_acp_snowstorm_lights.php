01d8189c192f09f83cb516c8136ada55e7b55b0e
Prosk8er <prosk8er@gotskillslounge.com>
2017-11-22 18:16:17 -0500

    small changes

--- language/en/info_acp_snowstorm_lights.php
+++ language/en/info_acp_snowstorm_lights.php
@@ -3,7 +3,7 @@
 *
 * Snowstorm & Lights extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2014 Prosk8er <http://www.gotskillslounge.com>
+* @copyright (c) 2017 Prosk8er <http://www.gotskillslounge.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
